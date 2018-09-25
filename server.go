c565aff68d449f84d4802726d75dd0fd2ee0681f
nicholasdaniel <nicholasjdaniel@gmail.com>
2018-09-25 13:14:09 -0400

    add web server

--- server.go
+++ server.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/codegangsta/negroni"
+	"github.com/njdaniel/token/routers"
+)
+
+func main() {
+	settings.Init()
+	router := routers.InitRoutes()
+	n := negroni.Classic()
+	n.UseHandler(router)
+	http.ListenAndServe(":5000", n)
 }
